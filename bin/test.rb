9296de0bd2a5e9b290ee19fe20519c486baf796a
Patrick <muilipato@gmail.com>
2023-05-30 17:29:43 +0300

    added require modules

--- bin/test.rb
+++ bin/test.rb
@@ -1,4 +1,10 @@
+
+require_relative '../lib/articles'
+require_relative '../lib/magazine'
+require_relative '../lib/author'
+
 # Create authors
+
 author1 = Author.new("Patrick Maweu")
 author2 = Author.new("Moringa School")
 
@@ -13,15 +19,15 @@ author2.add_article(magazine1, "Spotify Playlist")
 
 # Test object relationship methods
 article = author1.articles.first
-puts article.author.name  # Output: John Doe
-puts article.magazine.name  # Output: Fashion Weekly
+puts article.author.name
+puts article.magazine.name
 
-puts author1.magazines.map { |magazine| magazine.name }  # Output: Fashion Weekly, Tech Review
-puts magazine1.contributors.map { |author| author.name }  # Output: John Doe, Jane Smith
+puts author1.magazines.map { |magazine| magazine.name }
+puts magazine1.contributors.map { |author| author.name }
 
 # Test associations and aggregate methods
 magazine = Magazine.find_by_name("Coolest Music")
-puts magazine.article_titles  # Output: Fashion Trends, Summer Fashion Essentials
-puts magazine.contributing_authors.map { |author| author.name }  # Output: John Doe
+puts magazine.article_titles
+puts magazine.contributing_authors.map { |author| author.name }
 
-puts author1.topic_areas  # Output: Fashion, Technology
+puts author1.topic_areas
